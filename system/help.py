878287d0fe1614e95d1bc6eed1f7805616162908
kev402 <ke43vn74@gmail.com>
2025-04-05 14:46:16 -0500

    Update help.py

--- system/help.py
+++ system/help.py
@@ -1,6 +1,6 @@
 def mensaje():
     print("""PythonOS, un subsistema en python para Windows - Creado por kev402  https://github.com/kev402 https://gitlab.com/kev402 
 HELP DE PYTHONOS 
-Si presenta errores o no sabe como empezar ingrese a https://kev402.github.io/pythonos-site/help.html 
+Si presenta errores o no sabe como empezar ingrese a https://kev402.github.io/pythonos-site/docs.html 
 Ahí encontrará las guías más importantes para el uso de PythonOS. Si no encuentra el artículo que de solución a su problema, busque en sitios de terceros. 
 Si desea actualizar, debe borrar lo viejo y poner lo nuevo manualmente, procurando no borrar sus archivos o apps ya que no seremos responsables de ello.""")
